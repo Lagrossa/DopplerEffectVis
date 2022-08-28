c0e15330402f7fb77fe4d5da9c1572a91c73e344
Solus <yannis2004@live.com>
2022-08-28 11:36:55 -0500

    Updated Files.

--- Assets/Radial.cs
+++ Assets/Radial.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Radial : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnDrawGizmos()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
