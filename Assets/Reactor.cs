abf9f5b33e4f9c837aa74a9366820a070d624d4d
Solus <yannis2004@live.com>
2022-08-30 11:48:13 -0500

    Update Reactor.cs

--- Assets/Reactor.cs
+++ Assets/Reactor.cs
@@ -6,7 +6,7 @@ public class Reactor : MonoBehaviour
 {
     public GameObject trigger;
 
-    private void Update()
+    private void OnDrawGizmos()
     {
         if(trigger.GetComponent<Radial>().radius >= (transform.position - trigger.transform.position).magnitude)
         {
