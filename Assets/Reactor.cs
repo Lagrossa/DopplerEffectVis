ecc1889bd4b6df193c8c7ab62eb1c7f31f787393
Solus <yannis2004@live.com>
2022-08-31 14:33:58 -0500

    Update Reactor.cs

--- Assets/Reactor.cs
+++ Assets/Reactor.cs
@@ -6,12 +6,21 @@ public class Reactor : MonoBehaviour
 {
     public GameObject trigger;
     public float radius;
+
+    //Frequency Manager
+    private float currentTick;
+    private float lastTick;
+    public float frequency;
     public float rangeOfViability;
+
     private void OnDrawGizmos()
     {
         if(trigger.GetComponent<Radial>().radius >= (transform.position - trigger.transform.position).magnitude - rangeOfViability && trigger.GetComponent<Radial>().radius <= (transform.position - trigger.transform.position).magnitude + rangeOfViability)
         {
             Gizmos.color = Color.green;
+            currentTick = Time.time;
+            frequency = lastTick - currentTick;
+            lastTick = currentTick;
         }
         else
         {
